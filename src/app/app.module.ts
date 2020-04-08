89ab1a6de4cb167cffe5da4f2925bc931506fc69
masa-matsu37 <masa.matsu0307@gmail.com>
2020-04-08 18:36:19 +0900

    Angularフロントエンド側作成

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NabvarComponent } from './common/navbar/navbar.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
