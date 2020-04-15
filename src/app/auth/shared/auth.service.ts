313f5a7217b89635dcedceb83983e383f76aa0d9
masa-matsu37 <masa.matsu0307@gmail.com>
2020-04-15 11:10:41 +0900

    Authフロントエンド部分作成

---
+++ src/app/auth/shared/auth.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable()
+export class AuthService {
+
+    constructor(private http: HttpClient) { }
+
+
+    // getProducts(): Observable<any> {
+    //     // proxy.conf.json
+    //     // return this.http.get('http://localhost:3001/api/v1/products')
+    //     return this.http.get('/api/v1/products')
+    // }
+
+    // getProductById( productId: string ): Observable<any>  {
+    //     return this.http.get('/api/v1/products/' + productId)
+    // }
+}
